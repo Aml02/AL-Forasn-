615f0fa28ce8c87bcfe9e584323a876e5a701411
Aml Saed <amlsaed02@gmail.com>
2018-12-22 22:48:18 +0200

    add animation and chatbox code

--- js/code.js
+++ js/code.js
@@ -19,3 +19,57 @@ document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(
   }
 }
 setInterval(clock, 1000);
+/***************************************************************************/
+ /*wow*/
+new WOW().init();
+/****************************************************************************/
+function randombg(){
+  var random= Math.floor(Math.random() * 3) + 0;
+  var bigSize = ["url('images/background/BACKGROUND.png')",
+                 "url('images/background/background2.png')",
+                 "url('images/background/background3.png')"
+                 ];
+  document.getElementById("random").style.backgroundImage=bigSize[random];
+}
+/**************************************************************************/
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+/***************************************************************************/
+  $('#featured').owlCarousel({
+  nav:true,
+  dots:false,
+  next:false,
+    loop:true,
+    margin:0,
+  autoplay:true,
+autoplayTimeout:5000,
+autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:7,
+            nav:true,
+            loop:false
+        }
+    }
+})
+  
+  
+  
+  
+  
+  
+  
+}
